f1c4dc9adbaf96f408319baf36cfae12c99195e6
giuseppe <placidagiuseppe@gmail.com>
2021-02-26 16:19:18 +0100

    bonus svolto anche con la funzione onclick

---
+++ bonus/assets/main.js
@@ -0,0 +1,33 @@
+
+
+ alert("Ciao visitatore oggi ho imparato il javascript beccati questo alert!!");
+
+ //FUNZIONE
+ function myFunction(){
+
+  // PRENDE IL NOME SCRITTO DAL FORM
+    var nome = document.getElementById('nome').value ;
+
+    console.log(nome);
+
+  // PRENDE IL COGNOME SCRITTO DAL FORM
+
+    var cognome = document.getElementById('cognome').value ;
+
+    console.log(cognome);
+
+  // PRENDE IL GIORNO IN CUI E' NATO SCRITTO DAL FORM
+
+    var data = document.getElementById('data').value ;
+
+    console.log(data);
+
+  // PRENDE IL COLORE PREFERITO SCRITTO DAL FORM
+
+    var colore = document.getElementById('colore').value ;
+
+    console.log(colore);
+
+
+    document.getElementById('informazioni').innerHTML = "Ecco tutti i dati che hai scritto. " + nome + " - " + cognome + " - " + data + " - " + colore + " - " + 21;
+}
